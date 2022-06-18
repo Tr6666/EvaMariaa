01cc62ad2804e9e501533a67741606c629e58d61
Tr6666 <106578051+Tr6666@users.noreply.github.com>
2022-06-18 20:01:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -64,7 +64,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", url=f'https://t.me/{temp.U_NAME}?start={ident}_{file_id}'
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", url=f'https://t.me/{temp.U_NAME}?start={pre}_{file_id}'
                 ),
             ]
             for file in files
@@ -73,11 +73,11 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file.file_name}", url=f'https://t.me/{temp.U_NAME}?start={ident}_{file_id}'
+                    text=f"{file.file_name}", url=f'https://t.me/{temp.U_NAME}?start={pre}_{file_id}'
                 ),
                 InlineKeyboardButton(
                     text=f"{get_size(file.file_size)}",
-                    url=f'https://t.me/{temp.U_NAME}?start={ident}_{file_id}',
+                    url=f'https://t.me/{temp.U_NAME}?start={pre}_{file_id}',
                 ),
             ]
             for file in files
