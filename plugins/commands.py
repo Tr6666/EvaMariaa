fc6d8f4e33dc8fad115b3db909b4ab04843eee81
Tr6666 <106578051+Tr6666@users.noreply.github.com>
2022-06-18 20:57:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,7 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton(🎬 𝖩𝗈𝗂𝗇 𝖳𝖺𝗆𝗂𝗅𝖱𝗈𝖺𝗋𝗌 𝖭𝗈𝗐 🎬', url=f'http://t.me/Tamil_Roars')
+            InlineKeyboardButton('🎬 𝖩𝗈𝗂𝗇 𝖳𝖺𝗆𝗂𝗅𝖱𝗈𝖺𝗋𝗌 𝖭𝗈𝗐 🎬', url=f'http://t.me/Tamil_Roars')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
