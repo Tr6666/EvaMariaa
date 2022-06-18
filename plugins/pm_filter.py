4a33585a7060d7abf0223f834a81b174bbbed758
Tr6666 <106578051+Tr6666@users.noreply.github.com>
2022-06-18 20:06:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -641,7 +641,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", url=f'https://t.me/{temp.U_NAME}?start={ident}_{file_id}'
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
@@ -651,11 +651,11 @@ async def auto_filter(client, msg, spoll=False):
             [
                 InlineKeyboardButton(
                     text=f"{file.file_name}",
-                    url=f'https://t.me/{temp.U_NAME}?start={ident}_{file_id}',
+                    callback_data=f'{pre}#{file.file_id}',
                 ),
                 InlineKeyboardButton(
                     text=f"{get_size(file.file_size)}",
-                    url=f'https://t.me/{temp.U_NAME}?start={ident}_{file_id}',
+                    callblack_button=f'{pre}#{file.file_id}',
                 ),
             ]
             for file in files
