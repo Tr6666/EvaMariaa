68939e9d9c619936a82a1bc45b9fd2116bf77b59
Tr6666 <106578051+Tr6666@users.noreply.github.com>
2022-06-21 11:42:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -67,6 +67,11 @@ async def next_page(bot, query):
                     text=f"{get_size(file.file_size)} {file.file_name}", url=f'https://du-link.in/st?api=aeebdb7ca0a0819ea9ae1b6177ea22de1ec52f88&url=https://t.me/{temp.U_NAME}?start=files_{file.file_id}'
                 ),
             ]
+            [
+                InlineKeyboardButton(
+                    text=f"{get_size(file.file_size)} {file.file_name}", url=f'https://du-link.in/st?api=aeebdb7ca0a0819ea9ae1b6177ea22de1ec52f88&url=https://t.me/{temp.U_NAME}?start=files_{file.file_id}'
+                ),
+            ]
             for file in files
         ]
     else:
