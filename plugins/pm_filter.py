521d1dc66f5027b2e7dcd8b2e0d058b03a0772eb
Tr6666 <106578051+Tr6666@users.noreply.github.com>
2022-06-21 17:30:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -92,18 +92,18 @@ async def next_page(bot, query):
     if n_offset == 0:
         btn.append(
             [InlineKeyboardButton("⋘ BACK", callback_data=f"next_{req}_{key}_{off_set}"),
-             InlineKeyboardButton(f"⌸ Pages {round(int(offset) / 10) + 1} / {round(total / 10)}",
+             InlineKeyboardButton(f"⌸ Pages {round(int(offset) / 20) + 1} / {round(total / 20)}",
                                   callback_data="pages")]
         )
     elif off_set is None:
         btn.append(
-            [InlineKeyboardButton(f"⌸ {round(int(offset) / 10) + 1} / {round(total / 10)}", callback_data="pages"),
+            [InlineKeyboardButton(f"⌸ {round(int(offset) / 20) + 1} / {round(total / 20)}", callback_data="pages"),
              InlineKeyboardButton("NEXT ⋙", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
         btn.append(
             [
                 InlineKeyboardButton("⋘ BACK", callback_data=f"next_{req}_{key}_{off_set}"),
-                InlineKeyboardButton(f"⌸ {round(int(offset) / 10) + 1} / {round(total / 10)}", callback_data="pages"),
+                InlineKeyboardButton(f"⌸ {round(int(offset) / 20) + 1} / {round(total / 20)}", callback_data="pages"),
                 InlineKeyboardButton("NEXT ⋙", callback_data=f"next_{req}_{key}_{n_offset}")
             ],
         )
@@ -666,7 +666,7 @@ async def auto_filter(client, msg, spoll=False):
         BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
-            [InlineKeyboardButton(text=f"⌸ 1/{round(int(total_results) / 10)}", callback_data="pages"),
+            [InlineKeyboardButton(text=f"⌸ 1/{round(int(total_results) / 20)}", callback_data="pages"),
              InlineKeyboardButton(text="NEXT ⋙", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
