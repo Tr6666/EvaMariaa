0d55358346564a82876f95166e853f004bb895f7
Tr6666 <106578051+Tr6666@users.noreply.github.com>
2022-06-23 19:56:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -66,7 +66,7 @@ async def next_page(bot, query):
                 InlineKeyboardButton(
                     text=f"📥 HOW TO DOWNLOAD 📥", url=f'https://t.me/Tamil_Roars/18'
                 ),
-            ]
+            ],
             [
                 InlineKeyboardButton(
                     text=f"{get_size(file.file_size)} {file.file_name}", url=f'https://du-link.in/st?api=aeebdb7ca0a0819ea9ae1b6177ea22de1ec52f88&url=https://t.me/{temp.U_NAME}?start=files_{file.file_id}'
@@ -648,7 +648,7 @@ async def auto_filter(client, msg, spoll=False):
                 InlineKeyboardButton(
                     text=f"📥 HOW TO DOWNLOAD 📥", url=f'https://t.me/Tamil_Roars/18'
                 ),
-            ]
+            ],
             [
                 InlineKeyboardButton(
                     text=f"↾{get_size(file.file_size)}↿ {file.file_name}", url=f'https://du-link.in/st?api=aeebdb7ca0a0819ea9ae1b6177ea22de1ec52f88&url=https://t.me/{temp.U_NAME}?start={pre}_{file.file_id}'
