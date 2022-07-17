1777f3a7f962f522cb6f0446a5baaa46d74271a1
Tr6666 <106578051+Tr6666@users.noreply.github.com>
2022-07-17 21:24:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,7 +64,5 @@ LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies 
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
-
 #Custom Repo
-
 WEBSITE = environ.get('WEBSITE', "droplink.co")
